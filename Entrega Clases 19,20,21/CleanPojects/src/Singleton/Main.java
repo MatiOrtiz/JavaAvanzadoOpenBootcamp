7ac90abdeb861536b0553fa53fde095f6319209a
MatiOrtiz <matiasnortiz.dev@gmail.com>
2023-03-08 13:09:41 -0300

    args implementados en main.java y Singleton.java

--- Entrega Clases 19,20,21/CleanPojects/src/Singleton/Main.java
+++ Entrega Clases 19,20,21/CleanPojects/src/Singleton/Main.java
@@ -5,22 +5,26 @@ import State.Configuracion;
 public class Main {
 
 	public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java Main <property1> <property2>");
+            System.exit(1);
+        }
+
         // Singleton
         Singleton singleton = Singleton.getInstance();
         singleton.setCount(15);
         Singleton singleton2 = Singleton.getInstance();
-    
+
         System.out.println("Valor: " + singleton.getCount() + ", en memoria: " + singleton);
         System.out.println("Valor: " + singleton2.getCount() + ", en memoria: " + singleton2);
-    
+
         // Configuracion
         Configuracion config = Configuracion.getInstance();
-        config.setProperty1("Personalized value");
-        config.setProperty2(25);
+        config.setProperty1(args[0]);
+        config.setProperty2(Integer.parseInt(args[1]));
         Configuracion config2 = Configuracion.getInstance();
-    
+
         System.out.println("Valor: " + config.getProperty1() + ", " + config.getProperty2() + ", en memoria: " + singleton);
         System.out.println("Valor: " + config2.getProperty1() + ", " + config2.getProperty2() + ", en memoria: " + singleton2);
     }
-
 }
--- Entrega Clases 19,20,21/CleanPojects/src/Singleton/Singleton.java
+++ Entrega Clases 19,20,21/CleanPojects/src/Singleton/Singleton.java
@@ -4,17 +4,21 @@ public class Singleton {
     
     private static Singleton instance;
     private int count;
-    
+
     private Singleton() {}
-    
-    public static Singleton getInstance() {
-        return instance == null ? instance = new Singleton() : instance;
+
+    public static Singleton getInstance(int initialValue) {
+        if (instance == null) {
+            instance = new Singleton();
+            instance.setCount(initialValue);
+        }
+        return instance;
     }
-    
+
     public void setCount(int count) {
         this.count = count;
     }
-    
+
     public int getCount() {
         return count;
     }
