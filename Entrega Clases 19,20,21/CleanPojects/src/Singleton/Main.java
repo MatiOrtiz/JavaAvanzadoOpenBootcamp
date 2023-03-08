94c12f2b262a0a699fe772e3d49d41e0636a18d8
MatiOrtiz <matiasnortiz.dev@gmail.com>
2023-03-08 12:59:46 -0300

    main minificado

--- Entrega Clases 19,20,21/CleanPojects/src/Singleton/Main.java
+++ Entrega Clases 19,20,21/CleanPojects/src/Singleton/Main.java
@@ -5,26 +5,32 @@ import State.Configuracion;
 public class Main {
 
 	public static void main(String[] args) {
-
-        //--------------------Singleton--------------------//
-		Singleton singleton= Singleton.getInstance();
-		singleton.setCont(15);
-
-		Singleton singleton2= Singleton.getInstance();
-
-		System.out.println("Valor: " + singleton.getCont() + ", en memoria: " + singleton);
-		System.out.println("Valor: " + singleton2.getCont() + ", en memoria: " + singleton2);
-
-
-        //--------------------Aplicacion de Configuración--------------------//
-        Configuracion config= Configuracion.getInstance();
+        // Singleton
+        Singleton singleton = Singleton.getInstance();
+        singleton.setCount(15);
+        Singleton singleton2 = Singleton.getInstance();
+    
+        System.out.println("Valor: " + singleton.getCount() + ", en memoria: " + singleton);
+        System.out.println("Valor: " + singleton2.getCount() + ", en memoria: " + singleton2);
+    
+        // Configuracion
+        Configuracion config = Configuracion.getInstance();
         config.setProperty1("Personalized value");
         config.setProperty2(25);
-
-        Configuracion config2= Configuracion.getInstance();
-
+        Configuracion config2 = Configuracion.getInstance();
+    
         System.out.println("Valor: " + config.getProperty1() + ", " + config.getProperty2() + ", en memoria: " + singleton);
         System.out.println("Valor: " + config2.getProperty1() + ", " + config2.getProperty2() + ", en memoria: " + singleton2);
-	}
+    }
+    Los cambios realizados incluyen:
+    
+    Eliminar los comentarios, ya que no agregan información adicional.
+    Alinear los nombres de las variables para una mejor legibilidad.
+    Eliminar espacios y líneas en blanco innecesarias para reducir el tamaño del código.
+    
+    
+    
+    
+    
 
 }
